4c791be4a61bd43de706a76f2f4f30bb885f564b
distinctm1nd <149516256+distinctm1nd@users.noreply.github.com>
2024-07-04 14:51:14 -0600

    Update ScenePalettes.h
    updated colors in scene palettes and added the rest of the main palettes

--- firmware/src/led/scene/ScenePalettes.h
+++ firmware/src/led/scene/ScenePalettes.h
@@ -5,97 +5,135 @@
 
 // Mad Hatter Palette
 const TProgmemPalette16 mad_hatter_p PROGMEM = {
-    CRGB::DarkOrange, CRGB::Black, CRGB::Red,   CRGB::Black, CRGB::GreenYellow, CRGB::Black,
-    CRGB::Black,      CRGB::Black, CRGB::Green, CRGB::Black, CRGB::Red,         CRGB::Black,
-    CRGB::Blue,       CRGB::Black, CRGB::Black, CRGB::Black
-
+    CRGB::DarkOrange, CRGB::Black, CRGB::DarkOrange, CRGB::Black,
+    CRGB::GreenYellow, CRGB::Black, CRGB::GreenYellow, CRGB::Black,
+    CRGB::Green, CRGB::Black, CRGB::Green, CRGB::Black,
+    CRGB::Blue, CRGB::Black, CRGB::Blue, CRGB::Black
+    
 };
 
 const TProgmemPalette16 mad_hatter_main_p PROGMEM = {
-    CRGB::DarkOrange, CRGB::Black, CRGB::Red,   CRGB::Black, CRGB::GreenYellow, CRGB::Black,
-    CRGB::Black,      CRGB::Black, CRGB::Green, CRGB::Black, CRGB::Red,         CRGB::Black,
-    CRGB::Blue,       CRGB::Black, CRGB::Black, CRGB::Black
-
+    CRGB::DarkOrange, CRGB::Black, CRGB::Red, CRGB::Black,
+    CRGB::GreenYellow, CRGB::Black, CRGB::GreenYellow, CRGB::Black,
+    CRGB::Green, CRGB::Black, CRGB::Red, CRGB::Black,
+    CRGB::Blue, CRGB::Black, CRGB::Blue, CRGB::Black
+    
 };
 
 // Celestial Clouds Palette
 const TProgmemPalette16 celestial_clouds_p PROGMEM = {
-    CRGB::Navy, CRGB::Black, CRGB::Navy, CRGB::Black, CRGB::Snow, CRGB::Black,
-    CRGB::Snow, CRGB::Black, CRGB::Navy, CRGB::Black, CRGB::Navy, CRGB::Black,
-    CRGB::Snow, CRGB::Black, CRGB::Snow, CRGB::Black};
+    CRGB::Navy, CRGB::Black, CRGB::Navy, CRGB::Black,
+    CRGB::Snow, CRGB::Black, CRGB::Snow, CRGB::Black,
+    CRGB::Navy, CRGB::Black, CRGB::Navy, CRGB::Black,
+    CRGB::Snow, CRGB::Black, CRGB::Snow, CRGB::Black
+    
+};
 
 const TProgmemPalette16 celestial_clouds_main_p PROGMEM = {
-    CRGB::Navy,  CRGB::Black, CRGB::Red,   CRGB::Black, CRGB::Navy, CRGB::Black,
-    CRGB::Black, CRGB::Black, CRGB::Navy,  CRGB::Black, CRGB::Red,  CRGB::Black,
-    CRGB::Navy,  CRGB::Black, CRGB::Black, CRGB::Black
-
+    CRGB::Navy, CRGB::Black, CRGB::Red, CRGB::Black,
+    CRGB::Navy, CRGB::Black, CRGB::Black, CRGB::Black,
+    CRGB::Navy, CRGB::Black, CRGB::Red, CRGB::Black,
+    CRGB::Navy, CRGB::Black, CRGB::Black, CRGB::Black
+    
 };
 
 // Gone to Hell Palette
 const TProgmemPalette16 gone_to_hell_p PROGMEM = {
-    CRGB::DarkRed, CRGB::Black, CRGB::Red,    CRGB::Black, CRGB::Maroon, CRGB::Black,
-    CRGB::Black,   CRGB::Black, CRGB::Orange, CRGB::Black, CRGB::Red,    CRGB::Black,
-    CRGB::Maroon,  CRGB::Black, CRGB::Black,  CRGB::Black
-
+    CRGB::DarkRed, CRGB::Black, CRGB::Red, CRGB::Black,
+    CRGB::Maroon, CRGB::Black, CRGB::Black, CRGB::Black,
+    CRGB::Orange, CRGB::Black, CRGB::Red, CRGB::Black,
+    CRGB::Maroon, CRGB::Black, CRGB::Black, CRGB::Black
+    
 };
 
 const TProgmemPalette16 gone_to_hell_main_p PROGMEM = {
-    CRGB::Maroon,     CRGB::Black, CRGB::Maroon, CRGB::Black, CRGB::DarkOrange, CRGB::Black,
-    CRGB::Maroon,     CRGB::Black, CRGB::Maroon, CRGB::Black, CRGB::Maroon,     CRGB::Black,
+    CRGB::Maroon, CRGB::Black, CRGB::Maroon, CRGB::Black,
+    CRGB::DarkOrange, CRGB::Black, CRGB::Maroon, CRGB::Black,
+    CRGB::Maroon, CRGB::Black, CRGB::Maroon, CRGB::Black,
     CRGB::DarkOrange, CRGB::Black, CRGB::Maroon, CRGB::Black
-
+    
 };
 
 // Secret Combinations Palette
 const TProgmemPalette16 secret_combinations_p PROGMEM = {
-    CRGB::Gold,        CRGB::Black, CRGB::Red,   CRGB::Black, CRGB::GreenYellow, CRGB::Black,
-    CRGB::Black,       CRGB::Black, CRGB::Gold,  CRGB::Black, CRGB::Red,         CRGB::Black,
-    CRGB::GreenYellow, CRGB::Black, CRGB::Black, CRGB::Black
+    CRGB::Gold, CRGB::Black, CRGB::Red, CRGB::Black,
+    CRGB::GreenYellow, CRGB::Black, CRGB::GreenYellow, CRGB::Black,
+    CRGB::Gold, CRGB::Black, CRGB::Red, CRGB::Black,
+    CRGB::GreenYellow, CRGB::Black, CRGB::GreenYellow, CRGB::Black
+    
+};
 
+const TProgmemPalette16 secret_combinations_main_p PROGMEM = {
+    CRGB::Gold, CRGB::Black, CRGB::Red, CRGB::Black,
+    CRGB::GreenYellow, CRGB::Black, CRGB::Red, CRGB::Black,
+    CRGB::Gold, CRGB::Black, CRGB::Red, CRGB::Black,
+    CRGB::GreenYellow, CRGB::Black, CRGB::Red, CRGB::Black
+    
 };
 
 // The Salamander Palette
 const TProgmemPalette16 the_salamander_p PROGMEM = {
-    CRGB::Chartreuse, CRGB::Black, CRGB::Red,         CRGB::Black, CRGB::DarkGreen, CRGB::Black,
-    CRGB::OliveDrab,  CRGB::Black, CRGB::ForestGreen, CRGB::Black, CRGB::Red,       CRGB::Black,
-    CRGB::LawnGreen,  CRGB::Black, CRGB::OliveDrab,   CRGB::Black
+    CRGB::Chartreuse, CRGB::Black, CRGB::DarkGreen, CRGB::Black,
+    CRGB::DarkGreen, CRGB::Black, CRGB::OliveDrab, CRGB::Black,
+    CRGB::ForestGreen, CRGB::Black, CRGB::DarkGreen, CRGB::Black,
+    CRGB::LawnGreen, CRGB::Black, CRGB::OliveDrab, CRGB::Black
+    
+};
 
+const TProgmemPalette16 the_salamander_main_p PROGMEM = {
+    CRGB::Chartreuse, CRGB::Black, CRGB::Red, CRGB::Black,
+    CRGB::DarkGreen, CRGB::Black, CRGB::OliveDrab, CRGB::Black,
+    CRGB::ForestGreen, CRGB::Black, CRGB::Red, CRGB::Black,
+    CRGB::LawnGreen, CRGB::Black, CRGB::OliveDrab, CRGB::Black
+    
 };
 
 // Seer Stones Palettes
 const TProgmemPalette16 seer_stones_p PROGMEM = {
-    CRGB::Black,  CRGB::Black, CRGB::Red,    CRGB::Black, CRGB::Black, CRGB::Black,
-    CRGB::Indigo, CRGB::Black, CRGB::Black,  CRGB::Black, CRGB::Red,   CRGB::Black,
-    CRGB::Black,  CRGB::Black, CRGB::Yellow, CRGB::Black
-
+    CRGB::Black, CRGB::Black, CRGB::Red, CRGB::Black,
+    CRGB::Black, CRGB::Black, CRGB::Indigo, CRGB::Black,
+    CRGB::Black, CRGB::Black, CRGB::Red, CRGB::Black,
+    CRGB::Black, CRGB::Black, CRGB::Yellow, CRGB::Black
+    
 };
 
 const TProgmemPalette16 seer_stones_main_p PROGMEM = {
-    CRGB::Indigo, CRGB::Black, CRGB::Yellow, CRGB::Black, CRGB::Red,    CRGB::Black,
-    CRGB::Black,  CRGB::Black, CRGB::Indigo, CRGB::Black, CRGB::Yellow, CRGB::Black,
-    CRGB::Red,    CRGB::Black, CRGB::Black,  CRGB::Black
-
+    CRGB::Indigo, CRGB::Black, CRGB::Yellow, CRGB::Black,
+    CRGB::Red, CRGB::Black, CRGB::Black, CRGB::Black,
+    CRGB::Indigo, CRGB::Black, CRGB::Yellow, CRGB::Black,
+    CRGB::Red, CRGB::Black, CRGB::Black, CRGB::Black
+    
 };
 
 // Drunk as Hell Palettes
 const TProgmemPalette16 drunk_as_hell_p PROGMEM = {
-    CRGB::Blue,      CRGB::Black, CRGB::DarkMagenta, CRGB::Black, CRGB::Gold,   CRGB::Black,
-    CRGB::LawnGreen, CRGB::Black, CRGB::OrangeRed,   CRGB::Black, CRGB::Yellow, CRGB::Black,
-    CRGB::Lime,      CRGB::Black, CRGB::LimeGreen,   CRGB::Black
-
+    CRGB::Blue, CRGB::Black, CRGB::DarkMagenta, CRGB::Black,
+    CRGB::Gold, CRGB::Black, CRGB::LawnGreen, CRGB::Black,
+    CRGB::OrangeRed, CRGB::Black, CRGB::Yellow, CRGB::Black,
+    CRGB::Lime, CRGB::Black, CRGB::LimeGreen, CRGB::Black
+    
 };
 
 const TProgmemPalette16 drunk_as_hell_main_p PROGMEM = {
-    CRGB::Black, CRGB::Black,  CRGB::Black, CRGB::Maroon, CRGB::Black, CRGB::Black,
-    CRGB::Black, CRGB::Maroon, CRGB::Black, CRGB::Black,  CRGB::Black, CRGB::Maroon,
-    CRGB::Black, CRGB::Black,  CRGB::Black, CRGB::Maroon
-
+    CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Maroon,
+    CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Maroon,
+    CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Maroon,
+    CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Maroon
+    
 };
 
 // DC32 Y2K Aesthetic Theme
 const TProgmemPalette16 dc32_y2k_aesthetic_p PROGMEM = {
-    CRGB::DarkOrange, CRGB::Black, CRGB::Red,           CRGB::Black, CRGB::Teal,     CRGB::Black,
-    CRGB::DarkOrange, CRGB::Black, CRGB::DarkSlateBlue, CRGB::Black, CRGB::DeepPink, CRGB::Black,
-    CRGB::DeepPink,   CRGB::Black, CRGB::Red,           CRGB::Black};
+    CRGB::DarkOrange, CRGB::Black, CRGB::Red, CRGB::Black,
+    CRGB::Teal, CRGB::Black, CRGB::DarkOrange, CRGB::Black,
+    CRGB::DarkSlateBlue, CRGB::Black, CRGB::DeepPink, CRGB::Black,
+    CRGB::DeepPink, CRGB::Black, CRGB::Red, CRGB::Black
+};
 
+const TProgmemPalette16 dc32_y2k_aesthetic_main_p PROGMEM = {
+    CRGB::DarkOrange, CRGB::Black, CRGB::Red, CRGB::Black,
+    CRGB::Teal, CRGB::Black, CRGB::DarkOrange, CRGB::Black,
+    CRGB::DarkSlateBlue, CRGB::Black, CRGB::DeepPink, CRGB::Black,
+    CRGB::DeepPink, CRGB::Black, CRGB::Red, CRGB::Black
+};
 #endif // SCENE_PALETTES_H
